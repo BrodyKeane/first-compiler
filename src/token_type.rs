44d73703e31e9cbfa98085e39d83057ad3c0ab57
brody <keanebrody8585@gmail.com>
2023-05-10 03:51:03 -0400

    added token type enum

---
+++ src/token_type.rs
@@ -0,0 +1,23 @@
+enum TokenType{
+    //One char tokens
+    OpenParen, CloseParen,
+    OpenBrace, CloseBrace,
+    Comma, Dot, Minus, Plus,
+    Semicolon, Slash, Star,
+
+    //Comparison Tokens
+    Equal, Equal_Equal,
+    Bang, Bang_Equal,
+    Greater, Greater_Equal,
+    Less, Less_Equal,
+       
+    //Literals
+    Identifier, String, Number,
+
+    //Keywords
+    If, Else, And, Or, True, False,
+    For, While, Let, Fn, Class, Return,
+    Nil, Print, Super, This,
+
+    EOF
+}
